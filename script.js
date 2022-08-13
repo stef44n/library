2dbc907736f6aa3196238a0ac8b2b39473b3d1e6
Stefan <stefanseifeddine@gmail.com>
2022-08-13 02:24:00 +0100

    Create addBookToLibrary function

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        // return console.log(`${title} by ${author}, ${pages} pages, ${read}`);
-        let sentence = `${title} by ${author}, ${pages} pages, ${read}`;
-        return sentence;
-    }
+    // this.info = function() {
+    //     // return console.log(`${title} by ${author}, ${pages} pages, ${read}`);
+    //     let sentence = `${title} by ${author}, ${pages} pages, ${read}`;
+    //     return sentence;
+    // }
 }
+
+Book.prototype.info = function() {
+    let sentence = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    return sentence;
+}
+
+let newBook = undefined;
+let count = -1;
+
+function addBookToLibrary() {
+    newBook = new Book(
+        prompt('Title'), 
+        prompt('Author'),
+        prompt('Pages'),
+        prompt('Read'));
+
+    count++;
+    
+    myLibrary.push(newBook);
+    return console.log(myLibrary[count].info());
+}
+
+// addBookToLibrary();
